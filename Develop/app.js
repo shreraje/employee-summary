9b0ab8efd564e16c3490ef6414b1f58de0f44e3a
Rajendra Shrestha <rajendrashr@gmail.com>
2020-10-06 00:01:17 -0700

    Added Intern role function to the team, create team.htm function

--- Develop/app.js
+++ Develop/app.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-// const Employee = require("./lib/Employee");
+const { inherits } = require("util");
 
 let team = [];
 
@@ -113,27 +113,47 @@ function addEngineer() {
         })
 }
 
+// Function to input for an Intern role
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the full name of an Intern!"
+
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the ID of an Intern!"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter an email address of an Intern!"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Please enter school name of an Intern!"
+        }
+    ])
+        .then(input => {
+            const intern = new Intern(input.name, input.id, input.email, input.school);
+            team.push(intern);
+            buildTeam();
+        })
+}
 buildTeam();
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Function to render team.html as per User choice
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function createHtml() {
+    // Create output directory if previously exist
+    if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR);
 
-// After you have your html, you"re now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    // Create team.html file
+    fs.writeFileSync(outputPath, render(team));
+}
+createHtml();
